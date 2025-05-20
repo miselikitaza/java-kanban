6bf900d639e3970bfd4ff9865e577463cd3006a5
User <mitinareg.@yandex.ru>
2025-05-20 22:20:51 +0300

    Переопределены методы класса Object

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -47,4 +47,29 @@ public class Task {
         this.taskStatus = taskStatus;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", taskStatus=" + taskStatus +
+                '}';
+
+    }
 }
