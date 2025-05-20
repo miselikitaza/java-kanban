d017bbec55778124433470cd09dda908145bfdb5
User <mitinareg.@yandex.ru>
2025-05-20 22:24:24 +0300

    Реализована логика хранения задач-эпиков

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -2,5 +2,30 @@ package tasks;
 
 import java.util.ArrayList;
 
-public class Epic {
+public class Epic extends tasks.Task {
+
+    private ArrayList<Integer> subtasks;
+
+    public Epic(String name, String description) {
+        super(name, description, TaskStatus.NEW);
+        this.subtasks = new ArrayList<>();
+    }
+
+    public Epic(int id, String name, String description) {
+        super(id, name, description, TaskStatus.NEW);
+        this.subtasks = new ArrayList<>();
+    }
+
+    public ArrayList<Integer> getSubtasks() {
+        return subtasks;
+    }
+
+
+    public void addSubtasks(int subtask) {
+        this.subtasks.add(subtask);
+    }
+
+    public void removeSubtasks(int subtask) {
+        this.subtasks.remove(Integer.valueOf(subtask));
+    }
 }
