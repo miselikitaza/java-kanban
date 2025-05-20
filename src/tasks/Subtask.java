d6efd1c88dc36a9d984a79f9b8729c183e098460
User <mitinareg.@yandex.ru>
2025-05-20 22:26:20 +0300

    Реализована логика по хранению подзадач

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -1,4 +1,32 @@
 package tasks;
 
-public class Subtask {
+public class Subtask extends tasks.Task {
+
+    private final int epicId;
+
+    public Subtask(String name, String description, TaskStatus taskStatus, int epicId) {
+        super(name, description, taskStatus);
+        this.epicId = epicId;
+    }
+
+    public Subtask(int id, String name, String description, TaskStatus taskStatus, int epicId) {
+        super(id, name, description, taskStatus);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", epicId=" + epicId +
+                '}';
+    }
 }
+
