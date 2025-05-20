e8ff12828906b72adf4feca1f77a43fa626a7c27
User <mitinareg.@yandex.ru>
2025-05-20 22:30:20 +0300

    Добавлены коллекции, констуктор и метод для генерации уникального id

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -8,4 +8,21 @@ import tasks.Subtask;
 import tasks.TaskStatus;
 
 public class TaskManager {
+
+    private final HashMap<Integer, Task> tasks;
+    private final HashMap<Integer, Epic> epics;
+    private final HashMap<Integer, Subtask> subtasks;
+    private static int id = 0;
+
+    public TaskManager() {
+        tasks = new HashMap<>();
+        epics = new HashMap<>();
+        subtasks = new HashMap<>();
+    }
+
+    private int generateId() {
+        return ++id;
+    }
+
+
 }
