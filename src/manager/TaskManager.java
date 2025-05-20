ea88daa5bc1e1569e738fe7c4d632f6dbf00e0dc
User <mitinareg.@yandex.ru>
2025-05-20 22:27:19 +0300

    Импортированы необходимые классы

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -1,4 +1,11 @@
 package manager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import tasks.Task;
+import tasks.Epic;
+import tasks.Subtask;
+import tasks.TaskStatus;
+
 public class TaskManager {
 }
