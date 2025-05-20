bee9c6f125624f36ce6f0b4db36a3c6cc811b847
User <mitinareg.@yandex.ru>
2025-05-20 22:19:23 +0300

    Реализована логика по хранению обычных задач

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -3,4 +3,48 @@ package tasks;
 import java.util.Objects;
 
 public class Task {
+
+    private int id;
+    private String name;
+    private String description;
+    private TaskStatus taskStatus;
+
+    public Task(String name, String description, TaskStatus taskStatus) {
+        this.name = name;
+        this.description = description;
+        this.taskStatus = taskStatus;
+    }
+
+    public Task(int id,String name, String description, TaskStatus taskStatus) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.taskStatus = taskStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return taskStatus;
+    }
+
+    public void setStatus(TaskStatus taskStatus) {
+
+        this.taskStatus = taskStatus;
+    }
+
 }
