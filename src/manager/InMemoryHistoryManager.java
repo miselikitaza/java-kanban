7b0c0f8cf6a1bdf0f13cd0d03a8e3016597cca6f
User <mitinareg.@yandex.ru>
2025-05-31 22:00:27 +0300

    Создан класс InMemoryHistoryManager, имплементирующий интерфейс HistoryManager

---
+++ src/manager/InMemoryHistoryManager.java
@@ -0,0 +1,26 @@
+package manager;
+
+import tasks.Task;
+
+import java.util.ArrayList;
+
+public class InMemoryHistoryManager implements HistoryManager {
+
+    private final ArrayList<Task> history = new ArrayList<>();
+
+    @Override
+    public void add(Task task) {
+        if (history.size() < 10) {
+            history.add(task);
+        } else {
+            history.remove(0);
+            history.add(task);
+        }
+    }
+
+    @Override
+    public ArrayList<Task> getHistory() {
+        return history;
+    }
+
+}
