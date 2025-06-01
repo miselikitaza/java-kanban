6a29e97dd707a5b379f807e45fd3fc3309bf7641
User <mitinareg.@yandex.ru>
2025-06-02 00:10:30 +0300

    Вынесен метод с аннотацией @BeforeAll

--- test/tasks/EpicTest.java
+++ test/tasks/EpicTest.java
@@ -3,25 +3,30 @@ package tasks;
 import static org.junit.jupiter.api.Assertions.*;
 
 import manager.InMemoryTaskManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class EpicTest {
 
-    InMemoryTaskManager taskManager = new InMemoryTaskManager();
+    static InMemoryTaskManager taskManager;
+    static Epic epic;
+
+    @BeforeAll
+    public static void create() {
+        taskManager = new InMemoryTaskManager();
+        epic = new Epic("Ремонт", "поклеить обои");
+        taskManager.createEpic(epic);
+    }
 
     @Test
     public void shouldBeOneEpicWhenIdsAreEqual() {
-        Epic epicOne = new Epic("Ремонт", "поклеить обои");
-        taskManager.createEpic(epicOne);
-        Epic epicTwo = new Epic(epicOne.getId(), "Ремонт", "положить ламинат");
+        Epic epicTwo = new Epic(epic.getId(), "Ремонт", "положить ламинат");
         taskManager.createEpic(epicTwo);
-        assertEquals(epicOne, epicTwo);
+        assertEquals(epic, epicTwo);
     }
 
     @Test
     public void ShouldReturnFalseWhenAddEpicToItself() {
-        Epic epic = new Epic("Боня", "подготовить собаку к соревнованиям");
-        taskManager.createEpic(epic);
         epic.addSubtasks(epic.getId());
         assertFalse(epic.getSubtasks().contains(epic.getId()));
     }
