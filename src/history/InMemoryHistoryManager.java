6cfdc5d9fe00eaf99390f715d14c32438db58be0
User <mitinareg.@yandex.ru>
2025-06-04 14:03:20 +0300

    Максимальный размер истории просмотров вынесен в константу

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 public class InMemoryHistoryManager implements HistoryManager {
 
     private final ArrayList<Task> history = new ArrayList<>();
+    private final static int MAX_SIZE = 10;
 
     @Override
     public void add(Task task) {
-        if (history.size() < 10) {
+        if (history.size() < MAX_SIZE) {
             history.add(task);
         } else {
-            history.remove(0);
+            history.removeFirst();
             history.add(task);
         }
     }
