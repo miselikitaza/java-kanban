0badadabf3697da95400e7188a5b23475d029154
User <mitinareg.@yandex.ru>
2025-07-01 16:39:24 +0300

    Добавить тесты: на удаление задач из истории, если мы удаляем их все из списка задач; на удаление подзадачи из истории, если мы удаляем ее эпик; на добавление всех задач в историю при использовании метода getAll()

--- test/history/InMemoryHistoryManagerTest.java
+++ test/history/InMemoryHistoryManagerTest.java
@@ -3,20 +3,33 @@ package history;
 import static org.junit.jupiter.api.Assertions.*;
 
 import manager.InMemoryTaskManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tasks.Epic;
+import tasks.Subtask;
 import tasks.Task;
 import tasks.TaskStatus;
 
 class InMemoryHistoryManagerTest {
 
-    InMemoryTaskManager historyManager;
+    static InMemoryTaskManager historyManager;
     Task task1;
     Task task2;
+    static Epic epic;
+    static Subtask subtask;
+
+    @BeforeAll
+    public static void createEpicWithSubtasks() {
+        historyManager = new InMemoryTaskManager();
+        epic = new Epic("Эпик", "эпик");
+        historyManager.createEpic(epic);
+        subtask = new Subtask("Подзадача", "подзадача", TaskStatus.NEW, epic.getId());
+        historyManager.createSubtask(subtask);
+    }
 
     @BeforeEach
     public void create() {
-        historyManager = new InMemoryTaskManager();
         task1 = new Task("Задача 1", "Описание 1", TaskStatus.NEW);
         historyManager.createTask(task1);
         task2 = new Task("Задача 2", "Описание 2", TaskStatus.NEW);
@@ -62,5 +75,37 @@ class InMemoryHistoryManagerTest {
         historyManager.getTaskById(task1.getId());
         assertEquals(historyManager.getHistory().getFirst(), task2);
         assertEquals(historyManager.getHistory().getLast(), task1);
+    }
+
+
+    @Test
+    public void shouldRemoveTheTaskFromTheHistoryWhenWeRemoveAllTasks() {
+        historyManager.getEpicById(epic.getId());
+        historyManager.deleteAllTasks();
+        assertFalse(historyManager.getHistory().contains(task1));
+        assertFalse(historyManager.getHistory().contains(task2));
+        assertTrue(historyManager.getHistory().contains(epic));
+    }
+
+    @Test
+    public void shouldRemoveASubtaskFromTheHistoryIfHerEpicIsRemoved() {
+        historyManager.getEpicById(epic.getId());
+        historyManager.getSubtaskById(subtask.getId());
+        historyManager.deleteEpicById(epic.getId());
+        assertFalse(historyManager.getHistory().contains(subtask));
+        assertFalse(historyManager.getHistory().contains(epic));
+        assertTrue(historyManager.getHistory().contains(task1));
+    }
+
+    @Test
+    public void shouldAddAllTasksToTheHistoryIfWeUsedGetAll() {
+        Task task3 = new Task("Задача 3", "Описание 3", TaskStatus.NEW);
+        historyManager.createTask(task3);
+        Task task4 = new Task("Задача 4", "Описание 4", TaskStatus.NEW);
+        historyManager.createTask(task4);
+
+        historyManager.getAllTasks();
+        assertTrue(historyManager.getHistory().contains(task3));
+        assertTrue(historyManager.getHistory().contains(task4));
     }
 }
