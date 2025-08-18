a1574afc384eb0925a84d77f28fa32355089cd5e
User <mitinareg.@yandex.ru>
2025-08-18 15:42:23 +0300

    Создать отдельное поле для хранения времени окончания выполнения задачи. Написать метод по расчету этого времени на основе имеющихся подзадач.

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -1,19 +1,23 @@
 package tasks;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Epic extends Task {
 
     private final List<Integer> subtasks;
+    private LocalDateTime endTime;
 
     public Epic(String name, String description) {
-        super(name, description, TaskStatus.NEW);
+        super(name, description, TaskStatus.NEW, null, Duration.ZERO);
         this.subtasks = new ArrayList<>();
     }
 
     public Epic(int id, String name, String description) {
-        super(id, name, description, TaskStatus.NEW);
+        super(id, name, description, TaskStatus.NEW, null, Duration.ZERO);
         this.subtasks = new ArrayList<>();
     }
 
@@ -32,13 +36,70 @@ public class Epic extends Task {
         this.subtasks.remove(Integer.valueOf(subtask));
     }
 
+    public void updateTime(Map<Integer, Subtask> allSubtasks) {
+        if (allSubtasks == null || subtasks.isEmpty()) {
+            this.startTime = null;
+            this.duration = Duration.ZERO;
+            endTime = null;
+            return;
+        }
+
+        LocalDateTime earliestStart = null;
+        LocalDateTime latestEnd = null;
+        Duration totalDuration = Duration.ZERO;
+
+        for (int subtaskId : subtasks) {
+            Subtask subtask = allSubtasks.get(subtaskId);
+
+            if (subtask.getStartTime() != null) {
+                if (earliestStart == null || subtask.getStartTime().isBefore(earliestStart)) {
+                    earliestStart = subtask.getStartTime();
+                }
+            }
+
+            LocalDateTime subtaskEnd = subtask.getEndTime();
+            if (subtaskEnd != null && (latestEnd == null || subtaskEnd.isAfter(latestEnd))) {
+                latestEnd = subtaskEnd;
+            }
+
+            if (subtask.getDuration() != null) {
+                totalDuration = totalDuration.plus(subtask.getDuration());
+            }
+        }
+
+        this.startTime = earliestStart;
+        this.duration = totalDuration;
+        this.endTime = latestEnd;
+    }
+
+    @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
     @Override
     public String toString() {
+        String startTimeString;
+        if (startTime == null) {
+            startTimeString = "null";
+        } else {
+            startTimeString = startTime.format(DataTimeFormatter.formatter);
+        }
+
+        String endTimeString;
+        if (endTime == null) {
+            endTimeString = "null";
+        } else {
+            endTimeString = endTime.format(DataTimeFormatter.formatter);
+        }
         return "Epic{" +
                 "id=" + getId() +
                 ", name='" + getName() + '\'' +
                 ", description='" + getDescription() + '\'' +
                 ", status=" + getStatus() +
+                ", startTime=" + startTimeString + '\'' +
+                ", duration=" + DataTimeFormatter.formatDuration(getDuration()) + '\'' +
+                ", endTime=" + endTimeString + '\'' +
                 ", subtasks=" + subtasks +
                 '}';
     }
