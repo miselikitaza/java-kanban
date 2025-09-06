200fb9b5e9216a33bd468d85f0df4561d9d7aebf
User <mitinareg.@yandex.ru>
2025-09-06 20:53:07 +0300

    Написать классы-тесты для проверки корректной работы сервера с запросами, связанными с историей задач и списком приоритетных задач

---
+++ test/http/HttpHistoryTest.java
@@ -0,0 +1,27 @@
+package http;
+
+import exceptions.NotFoundException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpHistoryTest extends HttpTaskServerTest {
+
+    @Test
+    public void canGetHistory() throws NotFoundException, IOException, InterruptedException {
+        manager.getTaskById(task.getId());
+        manager.getSubtaskById(subtask.getId());
+        URI url = URI.create("http://localhost:8080/history");
+        request = HttpRequest.newBuilder().uri(url).GET().build();
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode());
+        assertTrue(response.body().contains("Задача 1"));
+        assertTrue(response.body().contains("Подзадача 1"));
+        System.out.println(response.body());
+    }
+}
